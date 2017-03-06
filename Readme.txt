f88f75e8b6119089f92a42e4021496a710ffe120
Stanislav Makhno <smakhno@softserveinc.com>
2017-03-06 13:36:55 +0200

    changed for heroku deployment

--- Readme.txt
+++ Readme.txt
@@ -5,7 +5,7 @@ For the client side were used: HTML, CSS, Bootstrap, jQuery, RequireJS, Mustache
 For the server side were used: Node.js, Express.js, MongoDB, Mongoose
  	
 To run at localhost:
-1. Install node js.
+1. Install node js and project dependencies.
 2. Access server folder through the command prompt.
 3. Enter 'node server' to run localhost server.
 4. Open any browser and enter 'http://localhost:80/' in the address line.
---
+++ index.js
@@ -0,0 +1,49 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var db         = require('server/utils/DbUtils');
+var app        = express();
+
+db.setUpConnection();
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+app.get('/projects', function(req, res){
+	db.getProjects().then(function(data){
+		res.send(data);
+ 	})
+});
+
+app.post('/projects', function(req, res){
+	db.createProject(req.body).then(function(data){
+		res.send(data);
+ 	});
+});
+
+app.post('/projectEdit', function(req, res){
+	db.editProjectTitle(req.body).then(function(data){
+		res.send(data);
+ 	});
+});
+
+app.post('/projectRemove', function(req, res){
+	db.deleteProject(req.body).then(function(data){
+		res.send(data);
+ 	});
+});
+
+app.post('/tasks', function(req, res){
+	db.updateTasks(req.body).then(function(data){
+		res.send(data);
+ 	});
+});
+
+
+app.listen(80);
+
+console.log('server started');
